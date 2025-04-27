78188b72a7d8ca2b9fa92b50b4333fb8e70d9a4f
RedTricity <lara.mcintyre@outlook.com>
2025-04-27 12:19:11 +0100

    removed user privacy

--- accounts/forms.py
+++ accounts/forms.py
@@ -81,11 +81,10 @@ class ProfileForm(forms.ModelForm):
         fields = [
             'bio', 'location', 'city', 'birth_date',
             'hearing_status', 'sign_language_preference',
-            'profile_picture', 'identity_tags', 'is_public', 
+            'identity_tags', 'profile_picture'
         ]
         widgets = {
-            'birth_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'birth_date': DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'location': CountrySelectWidget(attrs={'class': 'form-control'}),
             'city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g., Glasgow or London'}),
-            'is_public': forms.CheckboxInput(attrs={'class': 'form-check-input'}), 
         }
